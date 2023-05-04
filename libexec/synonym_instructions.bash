4c16c04bc1aee4d17fd728d8e6b8e8a606fe7ed2
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-05-04 08:24:20 -0700

    sketching out different outputs for different type of instructions

--- libexec/synonym_instructions.bash
+++ libexec/synonym_instructions.bash
@@ -4,22 +4,35 @@
 #   these parameters may alredy have commas from the
 #   users input
 
+function synonym_on () {
+  printf "$1 # is a synonym for: "
+}
+function synonym_off () {
+  :
+}
 
 function li () {
-    synonym_on "li \$$(name $1), \"$2\""
-    addiu "$1" $zero "$2"
-    synonym_off
+    _dst=$(name $(sed 's/,$//' <<< "$1" ))
+    _src="$2"
+    _text=$(sed 's/ //g' <<< "$2" )
+
+    [[ $_src != $_text ]] && _text="\"$_src\""
+    
+    echo "Synonym for: addi \$$_dst, \$zero, $_text"
+    addiu "$_dst", $zero, "$_src"
 }
 
 function move () {
-    synonym_on "move \$$(name $1), \$$(name $2)"
-    addu $1 $zero "$2"
+    _dst=$(name $(sed 's/,$//' <<< "$1" ))
+    _src=$(name $2)
+    
+    echo "Synonym for: addu \$$_dst, \$zero, \$$_src"
+    addu $_dst, $zero, "$_src"
     synonym_off
 }
 
 function nop () {
-    synonym_on "nop"
+    echo "Synonym for: ssl \$zero, \$zero, 0"
     sll $zero, $zero, 0
-    synonym_off
 }
 
