0676ba6265f913a0443da14bb576d69fa57b1733
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-05-10 13:34:56 -0700

    updated RRR instructions to follow the pattern

--- libexec/native_instructions.bash
+++ libexec/native_instructions.bash
@@ -1,17 +1,17 @@
 # Plan..  add "trap_on_V" command if a "signed" command
 
 ## ARITHMETIC
-alias   add="trap_on_V; execute_RRR +"
-alias  addu="execute_RRR +"
-alias  addi="trap_on_V; execute_RRI +"
-alias addiu="execute_RRI +"
-alias   sub="trap_on_V; execute_RRR -"
-alias  subu="execute_RRR -"
+alias   add="trap_on_V; execute_RRR add +"
+alias  addu="execute_RRR addu +"
+alias  addi="trap_on_V; execute_RRI addi +"
+alias addiu="execute_RRI addiu +"
+alias   sub="trap_on_V; execute_RRR sub -"
+alias  subu="execute_RRR subu -"
 
 # LOGICAL
-alias   and="execute_RRR \&"
-alias    or="execute_RRR \|"
-alias   xor="execute_RRR ^"
+alias   and="execute_RRR and \&"
+alias    or="execute_RRR or \|"
+alias   xor="execute_RRR xor ^"
 alias   nor="execute_nor nor"
 
 alias  andi="execute_RRI \&"
@@ -20,24 +20,24 @@ alias  xori="execute_RRI ^"
 alias  nori="nori"                      # command does not exist
 
 # SHIFT
-alias   sll="execute_RRI '<<'"
-alias   sra="execute_RRI '>>'"
+alias   sll="execute_RRI sll '<<'"
+alias   sra="execute_RRI sll '>>'"
 
-alias  sllv="execute_RRR '<<'"
-alias  srav="execute_RRR '>>'"
+alias  sllv="execute_RRR sslv '<<'"
+alias  srav="execute_RRR srav '>>'"
 
-alias   srl="execute_srl '>>>'"
-alias  srlv="execute_srlv '>>>'"
+alias   srl="execute_srl srl '>>>'"
+alias  srlv="execute_srlv srlv '>>>'"
 
 
 # MULT-DIV
-alias   mult="execute_MD \*"
-alias    div="execute_MD /"
+alias   mult="execute_MD mult \*"
+alias    div="execute_MD div /"
 
 # SPECIAL
-alias  mthi="execute_RR move _hi"       # mfhi $t0
-alias  mtlo="execute_RR move _lo"       # mflo $t0
+alias  mthi="execute_RR mthi move _hi"       # mfhi $t0
+alias  mtlo="execute_RR mtlo move _lo"       # mflo $t0
 
-alias  mfhi="reverse_op move _hi"       # mthi $t0
-alias  mflo="reverse_op move _lo"       # mtlo $t0
+alias  mfhi="reverse_op mfhi move _hi"       # mthi $t0
+alias  mflo="reverse_op mflo move _lo"       # mtlo $t0
 alias   lui="lui"
