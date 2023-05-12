eedf20af60505c63848cd342abd1625c8a0c74e6
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-05-12 09:24:19 -0700

    restructure to have a subdir for the machine

--- libexec/machine/encodings.bash
+++ libexec/machine/encodings.bash
@@ -1,20 +1,5 @@
 #! /bin/bash
 
-
-################################################
-# Op Encodings
-# Special  00
-# Special2 1C
-function lookup_opcode () {
-    local num="" 
-    num=$(eval echo \$op_code_$1)  2>&1 >/dev/null
-    if [[ $? == 0 ]] ; then 
-      echo $num
-    else
-      error "Undefined \"op\""
-    fi
-}
-
 # 000 Row
 declare -r        op_code_REG='000000'  # 0x00
 # declare -r       op_code_xx='000001'  
@@ -76,19 +61,6 @@ declare -r        op_code_sw='101011'  # 0x2B
 # declare -r      op_code_xx='101101'
 # declare -r      op_code_xx='101110'
 # declare -r      op_code_xx='101111'
-
-
-################################################
-# Func Encodings
-function lookup_func () {
-    local num="" 
-    num=$(eval echo \$func_code_$1)  2>&1 >/dev/null
-    if [[ $? == 0 ]] ; then
-      echo $num
-    else
-      error "Undefined \"func\""
-    fi 
-}
 
 # 000 Row
 declare -r     func_code_sll='000000'  # 0x00
@@ -152,70 +124,3 @@ declare -r    func_code_sltu='101011'  # 0x2B
 # declare -r    func_code_xx='101101'
 # declare -r    func_code_xx='101110'
 # declare -r    func_code_xx='101111'
-
-function encode_register () {
-   local _reg=$1
-   local _code=$(to_binary $(to_hex 2 $_reg))
-   local _code=$(sed -e 's/ //g' -e 's/.*\(.....\)$/\1/' <<< $_code )
-   echo $_code
-}
-alias encode_shamt=encode_register
-
-function encode_immediate () {
-   local _value=$1
-   local _value=$(( $1 & 0xFFFF ))
-   local _code=$(to_binary "$(to_hex 4 $_value)")
-   sed -e 's/ //g' <<< $_code 
-}
-
-function encode_address () {
-     local _label=$1
-     #local label="echo $(( 0x04000000))"
-     local _address=$(( $label >> 2 ))
-     echo $(to_binary "$(to_hex 8 $_address)")
-}
-
-emit_encodings=TRUE
-function print_R_encoding () {
-    [[ ${emit_encodings} == "TRUE" ]] || return
-
-    local _name="$1"
-    local _op_code="${op_code_REG}"
-    local _rs_code="$(encode_register $2)"
-    local _rt_code="$(encode_register $3)"
-    local _rd_code="$(encode_register $4)"
-    local _shamt=$(encode_shamt $5)
-    local _func_code="$(lookup_func $1)" 
-
-    printf "\t|%6s|%5s|%5s|%5s|%5s|%6s|\n" "op" "rs" "rt" "rd" "shamt" "func"
-    printf "\t|%s|%s|%s|%s|%s|%s|\n" \
-           $_op_code $_rs_code $_rt_code $_rd_code $_shamt $_func_code
-    printf "\n"
-}
-
-function print_I_encoding () {
-    [[ ${emit_encodings} == "TRUE" ]] || return
-
-    local _op=$(lookup_opcode $1) 
-    local _rs_code=$(encode_register $2)
-    local _rt_code=$(encode_register $3)
-    local _imm=$(encode_immediate $4)
-
-    printf "\t|%6s|%5s|%5s|%16s|\n" \
-            "op" "rs" "rt" "imm"
-    printf "\t|%s|%s|%s|%s|\n" \
-            $_op $_rs_code $_rt_code $_imm
-    printf "\n"
-}
-
-function print_J_encoding () {
-    [[ ${emit_encodings} == "TRUE" ]] || return
-
-    _op=$(lookup_opcode $1) 
-    _addr=$(encode_address $2)
-
-    printf "\t|%6s|%16s|\n" "op" "addr"
-    printf "\t|%s|%s|\n" "$_op" "$_addr"
-    printf "\n"
-}
-
