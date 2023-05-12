257d37941d907b516972784b9a536078c11191a1
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-05-12 13:27:37 -0700

    added more test cases

--- examples/mips.s
+++ examples/mips.s
@@ -8,6 +8,24 @@ nor $s6, $s7, $gp
 addi $t0, $t1, 5
 subi $t2, $t3, 5
 
+assign $t1 0x7FFFFFFFFF  # overflow
+addi $t1, $t1, 1
+
+assign $t1 0x8000000000  # overflow
+addi $t1, $t1, 1
+
+
+assign $t1 0xFFFFFFFFFF  # largest number
+srl $t2, $t1, 1     #0x7FFFFFFFF 
+sra $t2, $t1, 1     #0xFFFFFFFFF, -1
+
+li $at, 1
+srlv $t2, $t1, $at  #0x7FFFFFFFF   
+srav $t2, $t1, $at  #0xFFFFFFFFF, -1
+
+sll $t2, $t1, 1     # no Carry
+sla $t2, $t1, $at   # no Carry
+
 sll $t4, $t5, 2
 srl $t5, $t6, 3
 sra $t7, $t8, 1
