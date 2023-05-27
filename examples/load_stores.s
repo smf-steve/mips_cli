9be1b6582e6cfc0e3c33b1f3d547f84aad42287a
Steven Fitzgerald <steve@my.csun.edu>
2023-05-27 09:28:45 -0700

    first cut at implementing la, with improved load_store test case

--- examples/load_stores.s
+++ examples/load_stores.s
@@ -1,11 +1,11 @@
-assign_data_label A 4
-assign_data_label B 4
-assign_data_label C 2
+assign_label data A
+allocate_data 4
+assign_label data B
+allocate_data 4
+assign_label data C
+allocate_data 4
+print_labels
 
 li $t1, 4
 la $t2, A
-sw $t1, 4($t2)    # writes to B
-
-alias data_label_A
-alias data_label_B
-alias data_label_C
+sw $t1, 4 ($t2)    # writes to B
--- libexec/pseudo_instructions.bash
+++ libexec/pseudo_instructions.bash
@@ -6,6 +6,26 @@ function sub_execute () {
    eval "${@}"
 }
 
+function upper() {
+    _label=$1
+    eval _value=${label_data_}${_label}
+    echo $((  (_value >> 16 ) & 0xFFFFFFFF ))
+}
+function lower() {
+    _label=$1
+
+    eval _value=${label_data_}${_label}
+    echo $((  _value & 0xFFFFFFFF ))
+}
+function la() {
+    echo "Address of A is: $((eval _value=${label_data_}${2}))"
+    echo "Pseudo instruction for:"
+    sub_execute lui \$at, upper($2)
+    sub_execute ori \$$(name $1), \$at, lower($2)
+    pseudo_off
+}
+
+
 function subi() {
     echo "Pseudo instruction for:"
     sub_execute addi \$at, \$zero, $3
