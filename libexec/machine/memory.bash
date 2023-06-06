dd70fae1860968b43a5b0e6492fd7ad53a3358f5
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-06-06 12:11:38 -0700

    moved memory settings

--- libexec/machine/memory.bash
+++ libexec/machine/memory.bash
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-ENDIANNESS="LITTLE"
-
-text_start=0x00400000
-text_next=$text_start
-text_end=0x0FFFFFFF
-
-data_start=0x10010000
-data_next=$data_start
-data_end=0x1003FFFF
-
-heap_start=$((data_end + 1))
-heap_ptr_address=$heap_start
-heap_next=$heap_start+4
-MEM[$heap_ptr_address]=$heap_next;
-
-stack_top=0x7FFFFFFF
-# $sp = stack_top    # does this point to the top element or
-# the program args shojuld be placed here
-
 declare -a MEM
 
 function allocate_data_memory() {
