0a864443c107c6b06dd0456eea1c7dc96103a293
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-06-14 12:15:36 -0700

    created a test case for loadi syntax: llo, lhi, li

---
+++ examples/loadi.s
@@ -0,0 +1,6 @@
+li $t1, 0xAAAA    # Should be 0xFFFFAAAA
+li $t2, 0x7FFF    # Should be 0x7FFF
+lui $t2, 0x7FFF   # Should be 0x7FFF0000
+llo $t3, 0xBEEF
+lhi $t3, 0xDEAD   # $t3 should be 0xDEAD_BEAF
+llo $t3, 0xBEEF   # $t3 should be 0xDEAD_BEAF
