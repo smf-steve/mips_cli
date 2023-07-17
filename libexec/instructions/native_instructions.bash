53a607c074325328d0de5ad06f2dd4b8acb53c77
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-07-17 15:57:42 -0700

    fixed transposition error

--- libexec/instructions/native_instructions.bash
+++ libexec/instructions/native_instructions.bash
@@ -18,8 +18,8 @@ alias      nor="execute_ArithLog nor  \~\|"
 alias     nori="echo Instruction does not exist"    
 
 # Set Instructions
-alias      stl='execute_ArithLog  stl   "<"'
-alias     sltu='execute_ArithLog  stlu  "<"'
+alias      slt='execute_ArithLog  slt   "<"'
+alias     sltu='execute_ArithLog  sltu  "<"'
 alias     slti='execute_ArithLogI slti  "<"'
 alias    sltiu='execute_ArithLogI sltiu "<"'
 
