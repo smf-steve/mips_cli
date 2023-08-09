da454b5dccaa1a99080d3a115df32a34636e874e
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-08-09 12:35:28 -0700

    added more detail

--- examples/data_segment.s
+++ examples/data_segment.s
@@ -1,15 +1,28 @@
-.asciiz "This is a string\n"
-.data 
-.word  0xDEFACE00
-.byte  0xAA
-.byte  0xBB
-.byte  0xCC
-.byte  0xDD
-.byte  0xFF
-.half  0x0101 
-.half  0x0202
-.half  0x0303
-.byte  0xAA
-.word  0xDEFACE00 
+        .data
+        .asciiz "This is a string\n"
+        .word  0xDEFACE00
+A:      .byte  0xAA
+B:      .byte  0xBB
+C:      .byte  0xCC
+D:      .byte  0xDD
+F:      .byte  0xFF
+H1:     .half  0x0101 
+H2:     .half  0x0202
+H3:     .half  0x0303
+SPACE:  .space 1
+DEFACE: .word  0xDEFACE00 
+
+AA: .half 24
+BB: .half 24
+CC: .half 24
+DD: .half 24
+
+
+AAAA: .word 48
+BBBB: .word 48
+CCCC: .word 48
+DDDD: .word 48
+#list_labels
+
 
 print_memory
