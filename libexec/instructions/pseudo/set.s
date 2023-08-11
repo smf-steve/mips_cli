8896bf9c379c780cde7a314117ab19610ded4bb6
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-08-11 12:10:58 -0700

    add comment not implemented

--- libexec/instructions/pseudo/set.s
+++ libexec/instructions/pseudo/set.s
@@ -11,27 +11,35 @@
 
 
 .pseudo seq %dst, %src1, %src2        # %dst = (%src1 == %src2)? 1 : 0;
+        nop     # Not Implemented
 .end_pseudo
 
 .pseudo sne %rdst, src1, %src2        # %dst = (%src1 != %src2)? 1 : 0;
+        nop     # Not Implemented
 .end_pseudo
 
 .pseudo sge %dst, %src1, %src2        # %dst = (%src1 >= %src2)? 1 : 0;
+        nop     # Not Implemented
 .end_pseudo
 
 .pseudo sgeu %dst, %src1, %src2       # %dst = (%src1 >= %src2)? 1 : 0;
+        nop     # Not Implemented
 .end_pseudo
 
 .pseudo sgt %rdst, %src1, %src2       # %dst = (%src1 > %src2)? 1 : 0;
+        nop     # Not Implemented
 .end_pseudo
 
 .pseudo sgtu %rdst, %src1, %src2      # %dst = (%src1 > %src2)? 1 : 0;
+        nop     # Not Implemented
 .end_pseudo
 
 .pseudo sle %rdst, %src1, %src2       # %dst = (%src1 <= %src2)? 1 : 0;
+        nop     # Not Implemented
 .end_pseudo
 
 .pseudo sleu %rdst, %src1, %src2      # %dst = (%src1 <= %src2)? 1 : 0;
+        nop     # Not Implemented
 .end_pseudo
 
 #.native slt %rdst, %src1, %src2       # %dst = (%src1 < %src2)? 1 : 0;
